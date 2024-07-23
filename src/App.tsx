f2a934adf66537f4607c7ec47f6c336fe843d3ba
shikshita23 <shikshitasubedi1@gmail.com>
2024-07-23 11:40:14 +0545

    try out

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from 'antd';
+function App() {
+
+  return (
+    <>
+      <Button type="primary">Primary Button</Button>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
